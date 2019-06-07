3d6d5627aa24d82ea1eafc70d432673906953917
MG <meg@meg.com>
2019-06-07 16:30:37 +0300

    Initial setup & configuration of components, dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Rooms from './components/Rooms';
+import SingleRoom from './components/SingleRoom';
+import Error from './components/Error';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </>
   );
-}
+};
 
 export default App;

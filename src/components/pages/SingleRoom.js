31fbfd559382e1e12e1f59e73fdc8d2a330d1e21
MG <meg@meg.com>
2019-06-08 14:42:45 +0300

    Initial setup for SingleRoom component

--- src/components/pages/SingleRoom.js
+++ src/components/pages/SingleRoom.js
@@ -1,7 +1,54 @@
 import React, { Component } from 'react';
+import defaultImg from '../../images/room-1.jpeg';
+import Landing from '../layout/Landing';
+import Banner from '../layout/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../../Context';
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultImg
+    };
+  }
+  static contextType = RoomContext;
+  // componentDidMount(){}
+
   render() {
-    return <div>hello from single room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <Landing landing="roomsShowcase">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Landing>
+    );
   }
 }

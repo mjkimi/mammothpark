f4b2161feba93bb2670d4b9b42be8fa9cb725650
MG <meg@meg.com>
2019-06-09 16:13:00 +0300

    Filter: room type

--- src/Context.js
+++ src/Context.js
@@ -56,14 +56,37 @@ class RoomProvider extends Component {
   }
 
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
     const name = event.target.name;
-    const value = event.target.value;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
 
-    console.log(type, name, value);
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
 
-  filterRooms = () => {};
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
+  };
 
   render() {
     return (
--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -2,7 +2,12 @@ import React, { useContext } from 'react';
 import { RoomContext } from '../Context';
 import Title from './Title';
 
-export default function RoomsList() {
+// get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+export default function RoomsList({ rooms }) {
   const context = useContext(RoomContext);
   const {
     handleChange,
@@ -17,5 +22,36 @@ export default function RoomsList() {
     pets
   } = context;
 
-  return <section className="filter-container" />;
+  // get unique types
+  let types = getUnique(rooms, 'type');
+  types = ['all', ...types];
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end select type */}
+      </form>
+    </section>
+  );
 }

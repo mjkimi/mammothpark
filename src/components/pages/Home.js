ba6d70b73f3044e24db7c0260414c262ee7aac8c
MG <meg@meg.com>
2019-06-15 19:33:41 +0300

    Setting up gallery of Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Landing from '../layout/Landing';
 import Banner from '../layout/Banner';
 import { Link } from 'react-router-dom';
-import Services from '../Services';
+// import Services from '../Services';
 import FeaturedRooms from '../FeaturedRooms';
+import Description from '../Description';
 
 const Home = () => {
   return (
     <>
       <Landing landing="defaultShowcase">
-        {/* <Banner title="luxurios rooms" info="deluxe rooms starting at $299">
-          <Link to="/rooms" className="btn-primary">
-            Our Rooms
-          </Link>
-        </Banner> */}
+        <div className="gallery-item">
+          <div className="gallery-item-header">
+            <span>adding color to breakfast</span>
+          </div>
+          <div className="gallery-item-description">
+            Book your stay with breakfast included
+          </div>
+          <div className="gallery-bottom">Get the most out of your stay</div>
+        </div>
       </Landing>
-      <Services />
+      <Description />
+      {/* <Services /> */}
       <FeaturedRooms />
     </>
   );
